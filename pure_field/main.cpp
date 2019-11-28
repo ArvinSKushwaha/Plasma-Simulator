8988dd589e741db627442af3a64b213253620576
Arvin Singh Kushwaha <arvin.singhk@gmail.com>
2019-11-28 17:03:07 -0500

    Removed file.txt (Unnecessary) and updated magneticField allocation.
    Increased simulation resolution

--- pure_field/main.cpp
+++ pure_field/main.cpp
@@ -5,7 +5,7 @@
 // Fundamental Constants
 #define dt 1e-12                            // s
 #define REAL_SIZE Vec3D(1, 1, 1)            // m
-constexpr SizeTuple SIM_RES = {3, 3, 3}; // dimensionless max of 13500 elements
+constexpr SizeTuple SIM_RES = {20, 10, 100}; // dimensionless max of 13500 elements
 
 // Spacing between elements on the matrix
 Vec3D spacing = Vec3D(REAL_SIZE.x / SIM_RES.x, REAL_SIZE.y / SIM_RES.y, REAL_SIZE.z / SIM_RES.z);
@@ -42,18 +42,9 @@ int main()
         {
             for(int k = 0; k < SIM_RES.z; ++k)
             {
-                if(i == 0 && sqrt(pow(j - SIM_RES.y, 2) + pow(k - SIM_RES.z, 2)) < 5)
-                {
-                    massDensity.data[i][j][k] = total_mass/141.0;
-                    velocity.data[i][j][k] = Vec3D(10000, 0, 0);
-                    temperature.data[i][j][k] = 1000;
-                }
-                else
-                {
-                    massDensity.data[i][j][k] = 0;
-                    velocity.data[i][j][k] = ZERO_VEC;
-                    temperature.data[i][j][k] = 0;
-                }
+                massDensity(i, j, k, total_mass/(SIM_RES.y * SIM_RES.z * SIM_RES.x));
+                velocity(i, j, k, Vec3D(10000, 0, 0));
+                temperature(i, j, k, 1000);
             }
         }
     }
@@ -81,8 +72,8 @@ int main()
                             r_prime = getPosition(i1, j1, k1) - getPosition(i2, j2, k2);
                             if(r_prime.normSq() != 0)
                             {
-                                // std::cout << r_prime.sstr() << std::endl;
-                                magneticField.data[i1][j1][k1] +=  velocity(i2, j2, k2).cross(r_prime) * (total_charge / total_mass) * massDensity(i2, j2, k2) * (MU_0/(4 * PI * pow(r_prime.norm(), 3)));
+                                // std::cout << Vec3D(i1, j1, k1).sstr() << " " << (velocity(i2, j2, k2).cross(r_prime) * (E_CHARGE/M_PROTON) * (MU_0/(4 * PI * pow(r_prime.norm(), 3)))*massDensity(i2, j2, k2)).sstr() << std::endl;
+                                magneticField.data[magneticField.index(i1, j1, k1)] +=  velocity(i2, j2, k2).cross(r_prime) * (E_CHARGE/M_PROTON) * massDensity(i2, j2, k2) * (MU_0/(4 * PI * pow(r_prime.norm(), 3)));
                             }
                         }
                     }
@@ -91,7 +82,6 @@ int main()
             }
         }
     }
-
     // Total Bytes of Storage
     // std::cout << sizeof(magneticField) + sizeof(velocity) + sizeof(massDensity) + sizeof(currentDensity) + sizeof(temperature) + sizeof(pressure) + sizeof(pressureGrad) + sizeof(udotgradus) + sizeof(udotgradu) << std::endl;
 
@@ -115,10 +105,9 @@ int main()
         pressure += ((velocity * pressureGrad) + (velocity.divergence(spacing) * pressure * adiabatic_index)) * -dt;
         temperature << pressure * (massDensity ^ (-1)) * (M_PROTON / (2 * k_B));
         t += dt;
-        if((int)(t/dt) % 10 == 0)
+        if((int)(t/dt) % 100 == 0)
         {
             std::cout << t << std::endl;
         }
-    } while (t < 1e-10);
-    magneticField.toFile("file.txt");
+    } while (t < 1e-9);
 }
